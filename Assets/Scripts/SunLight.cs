cc3ca4b00e4785390d4c2dd068154576a98f9ba5
Jack Riales <jack@thatnaughtypanda.com>
2015-10-15 11:06:16 -0600

    Added sunlight.

---
+++ Assets/Scripts/SunLight.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SunLight : MonoBehaviour {
+
+	public Light lightReference;
+
+	// How long do days last (seconds)
+	public float dayTime = 480f;
+
+	// How long do nights last (seconds)
+	public float nightTime = 300f;
+
+	// Is it daytime or nighttime right now
+	private bool isDaytime = true;
+
+	// Internal timer
+	public float timer;
+
+	/*
+	===================================
+	 */
+
+	void Awake() {
+		// Handle null light reference if possible
+		if (!lightReference && GetComponent<Light> ())
+			lightReference = GetComponent<Light>();
+
+		// Set timer
+		if (isDaytime)
+			timer = dayTime;
+		else
+			timer = nightTime;
+	}
+
+	void Update() {
+		// Increment and check the time
+		checkDayTime ();
+
+		// Control the lights
+		if (isDaytime && !lightReference.enabled) {
+			lightReference.enabled = true;
+		} 
+
+		else if (!isDaytime && lightReference.enabled) {
+			lightReference.enabled = false;
+		}
+
+		// Do shit like sunsets here using lerp
+	}
+
+	/*
+	===================================
+	 */
+
+	public int getCurrentTimeSeconds() {
+		return Mathf.FloorToInt (timer);
+	}
+
+	public int getCurrentTimeMinutes() {
+		return Mathf.FloorToInt (timer / 60f);
+	}
+
+	private void checkDayTime() {
+		// Increment the time down frame delta-time
+		timer -= Time.deltaTime;
+
+		// If the timer's reached zero, it's time to change states
+		if (timer <= 0) {
+			isDaytime = !isDaytime;
+			if (isDaytime)
+				timer = dayTime;
+			else
+				timer = nightTime;
+		}
+	}
+}
--- Assets/Scripts/WorldController.cs
+++ Assets/Scripts/WorldController.cs
@@ -3,17 +3,23 @@ using System.Collections;
 
 public class WorldController : MonoBehaviour {
 
+	// Map array of blocks defining the world
+	private Block[][] _world;
+
 	// Define general dimensions for the world in blocks. Actual block count will vary with size of blocks.
 	public int worldWidth;
 	public int worldHeight;
 
+	// Block parent transform
+	public Transform blockParent;
+
 	// Used as the base for blocks
-	public Block blockPrefab;
+	public GameObject blockPrefab;
 
 	// Base block scale.
 	private int blockSize;
 
-	// ((worldWidth * worldHeight) / blockSize) cache
+	// (worldWidth * worldHeight) cache
 	private int blockCount;
 
 	/*
@@ -41,7 +47,7 @@ public class WorldController : MonoBehaviour {
 		blockSize = Mathf.FloorToInt((blockPrefab.transform.localScale.x + blockPrefab.transform.localScale.y) / 2);
 
 		// Set up block count total
-		blockCount = Mathf.FloorToInt((worldWidth * worldHeight) / blockSize);
+		blockCount = (worldWidth * worldHeight);
 	}
 
 	void Start () {
@@ -57,6 +63,26 @@ public class WorldController : MonoBehaviour {
 	 */
 
 	private void generateWorld() {
-	
+		// Columns 
+		for (int y = 0; y < worldHeight; y++) {
+
+			// Rows
+			for (int x = 0; x < worldWidth; x++) {
+
+				// Generate a block
+				GameObject newBlock = (GameObject) Instantiate (
+					blockPrefab, 
+					new Vector3(transform.position.x + x + blockSize, transform.position.y * -1 + y + blockSize, transform.position.z), 
+					Quaternion.identity);
+
+				// Set parent
+				if (blockParent)
+					newBlock.transform.parent = blockParent;
+				else
+					newBlock.transform.parent = this.transform;
+			}
+
+		}
 	}
+
 }
