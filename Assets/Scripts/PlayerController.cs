cec2d0ef8f033f2d82d9d79b3f47ceec0af2f0c7
Jack Riales <jack@thatnaughtypanda.com>
2015-11-30 11:43:16 -0600

    Fixed the gun rotation.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,31 +9,39 @@ public class PlayerController : MonoBehaviour {
 	public float moveSpeed;
 	public float shotForce;
 	public float bulletLifeTime;
-	private float gunAngle = 0.0f;
+	private Vector3 gunOffset;
+
+	protected void Start() {
+		gunOffset = (gun.transform.position - this.transform.position);
+	}
+
+	protected void Update() {
+		// Rotate the gun around the player given the mouse position		
+		Vector3 centerScreenPos = Camera.main.WorldToScreenPoint (transform.position);
+		Vector3 dir = Input.mousePosition - centerScreenPos;
+		float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+		Quaternion q = Quaternion.AngleAxis (angle, Vector3.forward);
+		gun.transform.position = transform.position + q * gunOffset;
+		gun.transform.rotation = q;
+	}
 
 	protected void FixedUpdate() {
+		// Move based on the horizontal input axis
 		GetComponent<Rigidbody> ().velocity = new Vector3 (
 			Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime,
 			GetComponent<Rigidbody> ().velocity.y,
 			0
 		);
 
+		// Jump functionality
 		/*if (Input.GetKeyDown(KeyCode.Space))
 			GetComponent<Rigidbody>().AddForce(new Vector3(0, jumpForce, 0));*/
 
-		// Rotate the gun around the player given the mouse position
-		var pos = Camera.main.WorldToScreenPoint(transform.position);
-		var dir = Input.mousePosition - pos;
-		var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
-		gun.transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
-
-		// TODO not quite right here.
-		gun.transform.RotateAround (transform.position, gun.transform.forward, angle);
-
 		// Shoot bullets where the gun is pointing
 		if (Input.GetMouseButtonDown (0)) {
-			GameObject newBullet = (GameObject) Instantiate (bullet, gun.transform.position + gun.transform.right, Quaternion.identity);
-			newBullet.GetComponent<Rigidbody>().AddForce(gun.transform.right * shotForce);
+			GameObject newBullet = (GameObject) Instantiate (bullet, gun.transform.position + gun.transform.up, Quaternion.identity);
+			newBullet.GetComponent<Rigidbody>().AddForce(gun.transform.up * shotForce);
+			GetComponent<Rigidbody>().AddForce(-gun.transform.up * shotForce);
 			GameObject.Destroy(newBullet, bulletLifeTime);
 		}
 	}
